5674dee5acfb02752bfbab2c3ed743f7ca62a198
Ajaychoudhary22 <ajayc498@gmail.com>
2025-09-09 15:30:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import VideoSection from "./components/VideoSection";
-import About from "./components/About";
-import Mission from "./components/Mission";
-import Initiatives from "./components/Initiatives";
-import Gallery from "./components/Gallery";
-import Testimonials from "./components/Testimonials";
-import Donate from "./components/Donate";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import VolunteerForm from "./components/VolunteerForm";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import VideoSection from "./Components/VideoSection";
+import About from "./Components/About";
+import Mission from "./Components/Mission";
+import Initiatives from "./Components/Initiatives";
+import Gallery from "./Components/Gallery";
+import Testimonials from "./Components/Testimonials";
+import Donate from "./Components/Donate";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import VolunteerForm from "./Components/VolunteerForm";
 
 export default function App() {
   const [showForm, setShowForm] = useState(false); // form visibility state
